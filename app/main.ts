560a30f85dc0158bc82ee5921aaa1e1186a6dfd2
OleksandrPap <oleksandr.pap@group107.com>
2026-05-21 00:54:09 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import { createInterface } from "readline";
 import path from "path";
 import fs from "fs";
+import { execSync } from "child_process";
 
 const rl = createInterface({
   input: process.stdin,
@@ -11,29 +12,33 @@ const rl = createInterface({
 const builtins = ["exit", "echo", "type"];
 const pathDirs = process.env.PATH?.split(path.delimiter) || [];
 
+const findExec = (cmd: string) => {
+  for (const dir of pathDirs) {
+    const fullPath = path.join(dir, cmd);
+    if (fs.existsSync(fullPath)) {
+      try {
+        fs.accessSync(fullPath, fs.constants.X_OK);
+        return fullPath;
+      } catch (err) {
+        continue;
+      }
+    }
+  }
+  return "";
+};
+
 rl.on("line", (command) => {
   if (command === "exit") return rl.close();
-  else if (command.startsWith("echo ")) console.log(command.slice(5));
   else if (command.startsWith("type ")) {
     const type = command.slice(5);
     if (builtins.includes(type)) console.log(`${type} is a shell builtin`);
     else {
-      let found = false;
-      for (const dir of pathDirs) {
-        const fullPath = path.join(dir, type);
-        if (fs.existsSync(fullPath)) {
-          try {
-            fs.accessSync(fullPath, fs.constants.X_OK);
-            console.log(`${type} is ${fullPath}`);
-            found = true;
-            break;
-          } catch (err) {
-            continue;
-          }
-        }
-      }
-      if (!found) console.log(`${type} not found`);
+      const path = findExec(type);
+      if (path) console.log(`${type} is ${path}`);
+      else console.log(`${type} not found`);
     }
+  } else if (findExec(command.split(" ")[0])) {
+    execSync(command, { stdio: "inherit" });
   } else console.log(`${command}: command not found`);
   rl.prompt();
 });
