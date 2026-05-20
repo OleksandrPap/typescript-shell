463c21023417bf15a6caa8b0f980176c01fb7cc4
OleksandrPap <oleksandr.pap@group107.com>
2026-05-21 00:09:36 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,12 +6,21 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
+const builtins = ["exit", "echo", "type"];
+
 rl.on("line", (command) => {
   if (command === "exit") {
     rl.close();
     return;
   } else if (command.startsWith("echo ")) {
     console.log(command.slice(5));
+  } else if (command.startsWith("type ")) {
+    const type = command.slice(5);
+    if (builtins.includes(type)) {
+      console.log(`${type} is a shell builtin`);
+    } else {
+      console.log(`${type} not found`);
+    }
   } else {
     console.log(`${command}: command not found`);
   }
